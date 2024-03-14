781d2f49feebf493fe99f3bfed12e0a8f90f92b3
Daniel <daniel.jaurellmevorach@student.ucll.be>
2024-03-14 21:34:39 +0100

    done with chap 1?

--- 03-regex/01-matching/27-is-valid-email-address/student.py
+++ 03-regex/01-matching/27-is-valid-email-address/student.py
@@ -1,1 +1,5 @@
 # Write your code here
+import re
+
+def is_valid_email_address(string):
+    return re.fullmatch(r"[0-9a-z.]{1,}@(ucll.be|student.ucll.be){1}", string)
--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,5 @@
 # Write your code here
+import re
+
+def is_valid_password(string):
+    return re.fullmatch(r"([0-9A-Za-z+-/*.@]{1,}){12}", string)
